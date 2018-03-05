444c4ff4d12382034d44c04bce902ce7dbeed54f
Henrik Enblom <henrik@enblom.com>
2018-03-05 22:16:41 +0100

    Tried something

--- twins-server.py
+++ twins-server.py
@@ -64,9 +64,9 @@ def identify(image):
 
     faces_encodings = face_recognition.face_encodings(image)
 
-    closest_distances = knn_clf.kneighbors(faces_encodings, n_neighbors=1)
+    closest_distances = knn_clf.kneighbors(faces_encodings, n_neighbors=2)
 
-    return knn_clf.predict(faces_encodings)[0]
+    return knn_clf.predict(faces_encodings)[1]
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=3001)
